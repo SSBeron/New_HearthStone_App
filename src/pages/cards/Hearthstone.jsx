72ccfa263fb69947420a583af5ea59cfd0a33cac
SSBeron <sultan.sagatov87@gmail.com>
2023-04-20 17:13:11 -0400

    push changes for render

--- src/pages/cards/Hearthstone.jsx
+++ src/pages/cards/Hearthstone.jsx
@@ -6,7 +6,7 @@ const [cards, setCards] = useState([])
 
   const options = {
     method: 'GET',
-    url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/info',
+    url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards',
     headers: {
       'X-RapidAPI-Key': '7e864495fdmsh342238470c2916cp1f09aajsn220cda1c2490',
       'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
@@ -30,25 +30,27 @@ useEffect(() => {
 }
 
 
-  return (
-  <div>
-    <h2>Hearthstone Information:</h2>
-    {cards.classes.map((card, index) => (
-      <div key={index}>
-        <h3>{card}</h3>
-        {/* <h3>Class: {card.classes}</h3> */}
-      </div>
-    ))}
-  </div>
-);
+
+
+ return (
+  
+ <div>
+  <h2>Hearthstone Information:</h2>
+  {cards && cards.Battlegrounds.filter((card) => 
+    card.img && card.type === "Hero" && 
+    !([85, 374, 494, 617, 682, 899, 955,].includes(cards.Battlegrounds.indexOf(card)))
+  ).map((card, index) => (
+    <div className="box" key={index}>
+      <h3>{card.name}</h3>
+      <h3>{cards.Battlegrounds.indexOf(card)}</h3>
+      <div><img src={card.img} alt="" /></div>
+    </div>
+  ))}
+</div>
+
+ );
 
 }
 
 export default HearthstoneInfo;
 
-
-{/* {cards.factions.map((card, index) => (
-  <div key={index}>
-    <h3>{card}</h3>
-  </div>
-))} */}
